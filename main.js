3f7877b66cc1d4cadf05116cba635220142059e4
albertev1 <147363770+albertev1@users.noreply.github.com>
2023-10-09 23:05:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
+
 const dado = document.getElementById('dado');
 const lanzarBtn = document.getElementById('lanzar');
 
-const imagenesDelDado = [
-    'dado1.png',
-    'dado2.png',
-    'dado3.png',
-    'dado4.png',
-    'dado5.png',
-    'dado6.png',
-];
-
+const carasDelDado = [1, 2, 3, 4, 5, 6];
 let intervalId;
-let caraVisible = 1; // Inicialmente mostramos la cara 1
 
 lanzarBtn.addEventListener('click', () => {
     clearInterval(intervalId);
 
     // Ocultar la cara actualmente visible
-    ocultarCara(caraVisible);
+    ocultarCara();
 
     // Obtener una nueva cara aleatoria
     const caraAleatoria = obtenerCaraAleatoria();
 
     // Mostrar la nueva cara
     mostrarCara(caraAleatoria);
-
-    caraVisible = caraAleatoria; // Actualizar la cara actualmente visible
 });
 
 function mostrarCara(cara) {
@@ -35,13 +25,15 @@ function mostrarCara(cara) {
     }
 }
 
-function ocultarCara(cara) {
-    const imagenOcultar = document.querySelector(`#cara${cara}`);
-    if (imagenOcultar) {
-        imagenOcultar.style.display = 'none';
-    }
+function ocultarCara() {
+    carasDelDado.forEach((cara) => {
+        const imagenOcultar = document.querySelector(`#cara${cara}`);
+        if (imagenOcultar) {
+            imagenOcultar.style.display = 'none';
+        }
+    });
 }
 
 function obtenerCaraAleatoria() {
-    return Math.floor(Math.random() * imagenesDelDado.length) + 1;
+    return carasDelDado[Math.floor(Math.random() * carasDelDado.length)];
 }
