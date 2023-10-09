0f884243bfeeef586d8d2e81034747e41c084386
albertev1 <147363770+albertev1@users.noreply.github.com>
2023-10-09 06:56:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-// Espera a que se cargue el contenido de la página
+// Espera a que se cargue completamente la página
 document.addEventListener("DOMContentLoaded", function () {
     // Obtén el elemento del tablero por su ID
     const tablero = document.getElementById("tablero");
 
-    // Establece el fondo del tablero como la imagen "fondo.jpg"
-    tablero.style.backgroundImage = "url('games/background.jpg')";
-    tablero.style.backgroundSize = "cover"; // Ajusta el tamaño del fondo según el contenedor
+    // Verifica si el elemento se encontró correctamente
+    if (tablero) {
+        // Establece el fondo del tablero como la imagen "fondo.jpg"
+        tablero.style.backgroundImage = "url('background.jpg')";
+        tablero.style.backgroundSize = "cover"; // Ajusta el tamaño del fondo según el contenedor
+    } else {
+        console.error("El elemento del tablero no se encontró en el documento.");
+    }
 
     // Ahora puedes continuar con la lógica de tu juego
-    // Por ejemplo, aquí puedes crear y posicionar las fichas del juego, etc.
 });
