be0d014e6aa18067a5f47b54b4df1493132f55e5
albertev1 <147363770+albertev1@users.noreply.github.com>
2023-10-10 17:00:11 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,33 +1,6 @@
-const dado = document.getElementById('dado');
-const lanzarBtn = document.getElementById('lanzar');
-const fichas = document.querySelectorAll('.ficha');
-
-const imagenesDelDado = [
-    'dado1.png',
-    'dado2.png',
-    'dado3.png',
-    'dado4.png',
-    'dado5.png',
-    'dado6.png',
-];
-
-let intervalId;
-let fichaArrastrada = null;
-
-lanzarBtn.addEventListener('click', () => {
-    clearInterval(intervalId);
-    intervalId = setInterval(() => {
-        const caraAleatoria = Math.floor(Math.random() * imagenesDelDado.length);
-        mostrarCara(imagenesDelDado[caraAleatoria]);
-    }, 100);
-    setTimeout(() => {
-        clearInterval(intervalId);
-    }, 2000); // Cambia este valor para ajustar la duración de la animación
-});
-
 function mostrarCara(imagen) {
-    const imagenes = document.querySelectorAll('.cara');
-    imagenes.forEach((imagenElement) => {
+    const imagenesCara = document.querySelectorAll('.cara');
+    imagenesCara.forEach((imagenElement) => {
         imagenElement.style.display = 'none';
     });
 
@@ -36,14 +9,3 @@ function mostrarCara(imagen) {
         imagenMostrar.style.display = 'block';
     }
 }
-
-fichas.forEach((ficha) => {
-    ficha.addEventListener('dragstart', (event) => {
-        fichaArrastrada = event.target;
-        // Agregar un efecto visual durante el arrastre (opcional)
-        fichaArrastrada.style.opacity = '0.6';
-    });
-
-    ficha.addEventListener('dragend', () => {
-        fichaArrastrada = null;
-        // Restaurar el estilo original al finalizar el arrastre
