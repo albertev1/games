74b09dd2d159109e4232c677af194f4630f942ad
albertev1 <147363770+albertev1@users.noreply.github.com>
2023-10-11 15:48:01 +0200

    Update main3.js

--- main3.js
+++ main3.js
@@ -6,48 +6,30 @@ function onDragStart(event) {
   // Almacenamos la posición inicial de la ficha
   ficha.startX = event.clientX;
   ficha.startY = event.clientY;
+
+  // Agregamos el evento `mousemove` a la ficha
+  ficha.addEventListener("mousemove", onMouseMove);
 }
 
 // Función para actualizar la posición de la ficha mientras se arrastra
-function onDrag(event) {
+function onMouseMove(event) {
   // Obtenemos la ficha que se está arrastrando
   const ficha = event.target;
 
   // Calculamos la nueva posición de la ficha
   ficha.style.left = event.clientX - ficha.startX + "px";
   ficha.style.top = event.clientY - ficha.startY + "px";
-
-  // Comprobamos si la ficha está dentro del tablero
-  if (ficha.style.left < 0) {
-    ficha.style.left = 0;
-  }
-
-  if (ficha.style.top < 0) {
-    ficha.style.top = 0;
-  }
-
-  if (ficha.style.left + ficha.clientWidth > document.querySelector(".tablero").clientWidth) {
-    ficha.style.left = document.querySelector(".tablero").clientWidth - ficha.clientWidth;
-  }
-
-  if (ficha.style.top + ficha.clientHeight > document.querySelector(".tablero").clientHeight) {
-    ficha.style.top = document.querySelector(".tablero").clientHeight - ficha.clientHeight;
-  }
 }
 
 // Función para detener el arrastre de la ficha
 function onDragEnd(event) {
-  // Obtenemos la ficha que se está arrastrando
+  // Eliminamos el evento `mousemove` de la ficha
   const ficha = event.target;
-
-  // Eliminamos la información de la posición inicial de la ficha
-  ficha.startX = null;
-  ficha.startY = null;
+  ficha.removeEventListener("mousemove", onMouseMove);
 }
 
 // Agregamos los eventos de arrastrar y soltar a las fichas
 document.querySelectorAll(".ficha").forEach((ficha) => {
   ficha.addEventListener("dragstart", onDragStart);
-  ficha.addEventListener("drag", onDrag);
   ficha.addEventListener("dragend", onDragEnd);
 });
